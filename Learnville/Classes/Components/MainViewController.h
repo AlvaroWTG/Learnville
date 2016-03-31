a2ad88b5978837a60df5a1bba763bdd2b6810cdb
Alvaro Garcia-Moya Herrera <agarcia@webtogo.de>
2016-03-31 17:32:47 +0200

    - MainVC. First steps implementing the main view controller.

---
+++ Learnville/Classes/Components/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  Learnville
+//
+//  Created by Alvaro on 31/3/16.
+//  Copyright © 2016 Alvaro GMH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ Learnville/Classes/Components/MainViewController.m
@@ -0,0 +1,36 @@
+//
+//  MainViewController.m
+//  Learnville
+//
+//  Created by Alvaro on 31/3/16.
+//  Copyright © 2016 Alvaro GMH. All rights reserved.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *viewHeader;
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UIView *viewOne;
+@property (weak, nonatomic) IBOutlet UIView *viewTwo;
+@property (weak, nonatomic) IBOutlet UIView *viewThree;
+@property (weak, nonatomic) IBOutlet UIView *viewFour;
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
